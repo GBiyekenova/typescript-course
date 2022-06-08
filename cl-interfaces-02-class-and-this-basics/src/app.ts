88b07d75f04a2b71fadd7dae54e9da59825b4fa9
Gulbike Biyekenova <gbknva@gmail.com>
2022-06-08 16:49:08 -0400

    private and public access modifiers

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -1,20 +1,29 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
 
   describe(this: Department) {
-    console.log('Department: ' + this.name);
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
-console.log(accounting)
+const accounting = new Department("Accounting");
 
 accounting.describe();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-
-accountingCopy.describe();
