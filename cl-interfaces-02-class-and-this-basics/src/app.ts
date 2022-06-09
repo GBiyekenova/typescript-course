1f9083e45eb90243cfba0bf995ccaef3614a1962
Gulbike Biyekenova <gbknva@gmail.com>
2022-06-09 15:15:50 -0400

    inheritance basics

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -1,8 +1,7 @@
 class Department {
   private employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
-  }
+  constructor(private readonly id: string, public name: string) {}
 
   describe(this: Department) {
     console.log(`Department ${this.id}: ${this.name} `);
@@ -18,10 +17,37 @@ class Department {
   }
 }
 
-const accounting = new Department("d1", "Accounting");
+class ITDepartment extends Department {
+  //admins: string[]; is declared in the constructor using public/private key
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT"); //super must be declared before any 'this' key
+    this.admins = admins;
+  }
+}
 
-accounting.describe();
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-accounting.printEmployeeInformation();
+const it = new ITDepartment("d1", ["Max"]);
 
+it.describe();
+
+it.addEmployee("Max");
+it.addEmployee("Manu");
+
+it.printEmployeeInformation();
+console.log(it);
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting"); //super must be declared before any 'this' key
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+const accounting = new AccountingDepartment('d2', [])
+accounting.addReport('Something went wrong');
+accounting.printReports();
