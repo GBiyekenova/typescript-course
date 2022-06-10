d2c243db8df736904617c81b5759271d182960fa
Gulbike Biyekenova <gbknva@gmail.com>
2022-06-10 11:11:39 -0400

    overriding properties & protected modifier

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {}
 
@@ -40,6 +40,13 @@ class AccountingDepartment extends Department {
     super(id, "Accounting"); //super must be declared before any 'this' key
   }
 
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -48,6 +55,9 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
-const accounting = new AccountingDepartment('d2', [])
-accounting.addReport('Something went wrong');
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Something went wrong");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 accounting.printReports();
+console.log(accounting)
