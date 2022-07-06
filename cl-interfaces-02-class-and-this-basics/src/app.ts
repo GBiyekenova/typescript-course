0538f134b9fcd76f75c4f6de04c168591ebcfb25
Gulbike Biyekenova <gbknva@gmail.com>
2022-07-06 10:56:55 -0400

    type guard -in-

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -29,3 +29,17 @@ function add1(a: Combinable, b: Combinable) {
   }
   return a + b;
 }
+
+type UnknownEmployee = Admin | Employee;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name " + emp.name)
+  if ('privileges' in emp) {
+    console.log('Privileges ' + emp.privileges)
+  } 
+  if ('startDate' in emp) {
+    console.log('Start Date ' + emp.startDate)
+  }
+}
+
+printEmployeeInformation(e1)
