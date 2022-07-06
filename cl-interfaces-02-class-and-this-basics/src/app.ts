565add4ef4a6e74b27be59cd9437634ab5bdcd0e
Gulbike Biyekenova <gbknva@gmail.com>
2022-07-06 10:47:35 -0400

    intersection types cont

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -22,3 +22,10 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add1(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
