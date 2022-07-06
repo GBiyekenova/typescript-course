9c47cf17caa2b37414a2c51d618b182ec77b3759
Gulbike Biyekenova <gbknva@gmail.com>
2022-07-06 11:09:08 -0400

    instanecof type guard

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -13,9 +13,9 @@ type Employee = {
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-  name: 'Max',
-  privileges: ['create-server'],
-  startDate: new Date()
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
 };
 
 type Combinable = string | number;
@@ -33,13 +33,43 @@ function add1(a: Combinable, b: Combinable) {
 type UnknownEmployee = Admin | Employee;
 
 function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log("Name " + emp.name)
-  if ('privileges' in emp) {
-    console.log('Privileges ' + emp.privileges)
-  } 
-  if ('startDate' in emp) {
-    console.log('Start Date ' + emp.startDate)
+  console.log("Name " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date " + emp.startDate);
   }
 }
 
-printEmployeeInformation(e1)
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a Truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo - " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
