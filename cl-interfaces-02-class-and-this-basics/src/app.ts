a3fc92395eb28b38ac09c756c6c4f1b493c77b8b
Gulbike Biyekenova <gbknva@gmail.com>
2022-07-11 11:24:32 -0400

    function overloads

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -23,12 +23,20 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+//function overloads
+function add1(a: string, b: string): string;
+function add1(a: number, b: number): number;
+function add1(a: string, b: number): string;
+function add1(a: number, b: string): string;
 function add1(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const result = add1("Shawn", " Max");
+result.split(' ');
 
 type UnknownEmployee = Admin | Employee;
 
